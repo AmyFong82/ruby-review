904ebb941f4fb460a1982eb8ae4b7a18f42607e2
AmyFong82 <amyhfong@gmail.com>
2021-11-15 09:28:01 -0500

    added example of class method (with "this") vs instance method

--- simpleRubymethods_and_classes.rb
+++ simpleRubymethods_and_classes.rb
@@ -55,3 +55,30 @@ greeter.name = "Betty"
 # attr_accessor allows reading and writing of the attribute. (Actual called: getter and setter)
 getter: name
 setter: name=
+
+
+class SayHello
+  def self.from_the_class
+    "Hello, from a class method"
+  end
+
+  def from_an_instance
+    "Hello, from an instance method"
+  end
+end
+
+>> SayHello.from_the_class
+=> "Hello, from a class method"
+
+>> SayHello.from_an_instance
+=> undefined method `from_an_instance' for SayHello:Class
+
+
+>> hello = SayHello.new
+>> hello.from_the_class
+=> undefined method `from_the_class' for #<SayHello:0x0000557920dac930>
+
+>> hello.from_an_instance
+=> "Hello, from an instance method"
+
+We cannot call an instance method on the class itself, and we cannot directly call a class method on an instance.
