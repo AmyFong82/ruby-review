61227de5c65ace6d0f2c6949100b6e75c54c8b7c
AmyFong82 <amyhfong@gmail.com>
2022-03-25 11:16:53 -0400

    added reverse_string.rb to this folder.

---
+++ ruby/reverse_string.rb
@@ -0,0 +1,26 @@
+# Please add your pseudocode to this file
+# 1. Convert the string into an array.
+# 2. Create an empty reversed_array placeholder for the reversed array.
+# 3. Get the length of the string.
+# 4. Make a counter with the length of the string - 1.
+# 5. Iterate over the array, and putting the 1st element to the last, with the help of the counter.
+# 6. Join the array back into string.
+
+
+def reverse_string(str)
+  # type your code in here
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Expecting: 'ih'"
+  puts "=>", reverse_string('hi')
+
+  puts
+
+  puts "Expecting: 'ybabtac'"
+  puts "=>", reverse_string('catbaby')
+
+  # Don't forget to add your own!
+end
+
